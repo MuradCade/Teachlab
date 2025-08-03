45d12f239f4e1bb12820fda90bf241f82233866d
Murad Cade <king12murad@gmail.com>
2025-08-03 13:35:34 +0300

    change env credentials in the env file inside the models folder

--- model/env.php
+++ model/env.php
@@ -11,27 +11,3 @@ define('PASSWORD', '');
 define('DATABASE_NAME', 'teachlabs');
 define('EMAIL_GOGLE_APP_SECRET_KEY', '');
 define('EMAIL', 'teachlabinc@gmail.com');
-// // software configuration
-// define('SOFTWARE_NAME', 'TeachLab');
-// define('VERSION', '1.2');
-
-// // Database Configuration
-// define('HOSTNAME', 'localhost');
-// define('USER_NAME', 'root');
-// define('PASSWORD', '');
-// define('DATABASE_NAME', 'teachlabs');
-// define('EMAIL_GOGLE_APP_SECRET_KEY', 'vpwjnvjyhuoidtmv');
-// define('EMAIL', 'king12murad@gmail.com');
-
-
-// software configuration
-//define('SOFTWARE_NAME', 'TeachLab');
-//define('VERSION', '1.2');
-
-// Database Configuration
-//define('HOSTNAME', 'localhost');
-//define('USER_NAME', 'root');
-//define('PASSWORD', '');
-//define('DATABASE_NAME', 'teachlabs');
-//define('EMAIL_GOGLE_APP_SECRET_KEY', 'vpwjnvjyhuoidtmv');
-//define('EMAIL', 'king12murad@gmail.com');
